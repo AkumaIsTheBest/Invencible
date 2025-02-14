00357e5a7751eabd3c67e4c4682588b3fbae1489
AkumaIsTheBest <robertocarlosslkjjjkk@gmail.com>
2025-02-14 10:32:45 -0400

    ANIMAÇÕES NOVAS NA INDEXXX

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.querySelector('.toggle-button');
     const optionsContainer = document.querySelector('.options-container');
+    const gif = document.querySelector('.gif');
+
+    let clickCount = 0;  
 
     toggleButton.addEventListener('click', function() {
-     
         optionsContainer.classList.toggle('hidden');
     });
 
-  
     const optionButtons = document.querySelectorAll('.option-button');
     optionButtons.forEach(button => {
         button.addEventListener('click', function() {
-            alert(`vc esta indo na pagina... ${button.textContent}!`);
+            alert(`Você está indo para a página de ${button.textContent}!`);
         });
+    });
+
+   
+    gif.addEventListener('click', function() {
+   
+        const gifs = [
+            '../ims/invincible-fortnite.gif',
+            '../ims/omni-man-pull-up.gif',
+            '../ims/omniman-fortnite.gif',
+            '../ims/invincible-omniman.gif',
+            '../ims/yoshikage-kira-evil-smile.gif'
+        ];
+
+        
+        gif.src = gifs[clickCount];  
+        clickCount++;  
+
+    
+        if (clickCount >= gifs.length) {
+            clickCount = 0;  
+        }
     });
 });
